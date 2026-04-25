eff9f034282d1f3a68ac71f264a72f36fa42c814
wanderson-del <wandersonlibra@gmail.com>
2026-04-25 16:16:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='vocabulario-vivo-v2';
+const CACHE_NAME='vocabulario-vivo-v3';
 const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE_NAME)return caches.delete(k)}))).then(()=>self.clients.claim()))});
